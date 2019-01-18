93ac20fc9f275617f447cf8ae49f089e99ae197c
Harrison Evans <hevans9000@gmail.com>
2019-01-18 10:17:46 +0000

    feat: custom cross-field validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,65 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'form-tests';
 
   methods = ['paypal', 'other'];
 
-  form: FormGroup = new FormGroup({
-    method: new FormControl('paypal', {
-      validators: [Validators.required]
-    }),
-    paypal: new FormGroup({
-      email: new FormControl(null, {
-        validators: [Validators.required, Validators.email]
+  form: FormGroup = new FormGroup(
+    {
+      method: new FormControl('paypal', {
+        validators: [Validators.required]
+      }),
+      paypal: new FormGroup({
+        email: new FormControl(null, {
+          validators: [Validators.email]
+        })
+      }),
+      other: new FormGroup({
+        email: new FormControl(null, {
+          validators: [Validators.email]
+        })
       })
-    }),
-    other: new FormGroup({
-      email: new FormControl(null, {
-        validators: [Validators.required, Validators.email]
-      })
-    })
-  });
+    },
+    { validators: customValidator }
+  );
+
+  ngOnInit() {
+    this.form.get('method').valueChanges.subscribe(() => {
+      this.form
+        .get('paypal')
+        .get('email')
+        .setValue(null);
+      this.form
+        .get('other')
+        .get('email')
+        .setValue(null);
+    });
+  }
 }
+
+export const customValidator: ValidatorFn = (
+  control: FormGroup
+): ValidationErrors | null => {
+  const method = control.get('method');
+  const paypal = control.get('paypal').get('email');
+  const other = control.get('other').get('email');
+
+  if ((paypal.value && paypal.valid) || (other.value && other.valid)) {
+    return null;
+  } else {
+    return { paymentSelectionInvalid: true };
+  }
+};
